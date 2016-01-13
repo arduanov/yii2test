6c6a828cba20b551fed6fb10f9c08913f2c1466b
Marsel <arduanov@gmail.com>
2016-01-13 10:03:01 +0500

    travis

---
+++ tests/codeception/fixtures/FeedbackFixture.php
@@ -0,0 +1,9 @@
+<?php
+namespace tests\codeception\fixtures;
+
+use yii\test\ActiveFixture;
+
+class FeedbackFixture extends ActiveFixture
+{
+    public $modelClass = 'app\models\Feedback';
+}
---
+++ tests/codeception/fixtures/data/feedback.php
@@ -0,0 +1,38 @@
+<?php
+return [
+    [
+        'subject' => 'other',
+        'body' => 'Nam omnis qui debitis sint repellendus repudiandae. Doloribus aliquid hic qui aut autem. Laborum autem aliquid et sed suscipit.',
+        'file' => '{"name":"test.jpg","mime":"image\\/jpeg","size":53785}',
+    ],
+    [
+        'subject' => 'technical',
+        'body' => 'Mollitia quisquam qui rerum ut eligendi pariatur. Voluptatem sequi repudiandae nam repellat quibusdam qui. Non soluta similique et.',
+        'file' => '{"name":"test.jpg","mime":"image\\/jpeg","size":53785}',
+    ],
+    [
+        'subject' => 'technical',
+        'body' => 'Repellendus maxime qui quia et placeat ullam dolorem. Cum eum laborum enim nam. Dolore et consequatur voluptate alias veniam perspiciatis.',
+        'file' => '{"name":"test.jpg","mime":"image\\/jpeg","size":53785}',
+    ],
+    [
+        'subject' => 'technical',
+        'body' => 'Ut vero sunt ipsa debitis veritatis numquam. Ipsum sapiente fugiat ad eveniet. Debitis rerum rerum quam sunt minima qui numquam. At qui quibusdam saepe eum ipsum dolor a molestiae.',
+        'file' => '{"name":"test.jpg","mime":"image\\/jpeg","size":53785}',
+    ],
+    [
+        'subject' => 'support',
+        'body' => 'Mollitia corporis consequuntur eius quo architecto. Similique qui rerum culpa eum autem. Ut nihil possimus dicta et recusandae. Quis aut omnis vel in ducimus impedit et incidunt.',
+        'file' => '{"name":"test.jpg","mime":"image\\/jpeg","size":53785}',
+    ],
+    [
+        'subject' => 'support',
+        'body' => 'Quidem velit illum quas accusamus. Earum voluptatem quasi aut porro debitis aperiam omnis debitis. Magni nostrum quos exercitationem.',
+        'file' => '{"name":"test.jpg","mime":"image\\/jpeg","size":53785}',
+    ],
+    [
+        'subject' => 'technical',
+        'body' => 'Dolor accusantium vel eos quae rem. Impedit et temporibus officiis officia. Nihil cum aut labore aliquid maiores dignissimos distinctio. Tempore consectetur sint voluptatibus.',
+        'file' => '{"name":"test.jpg","mime":"image\\/jpeg","size":53785}',
+    ],
+];
